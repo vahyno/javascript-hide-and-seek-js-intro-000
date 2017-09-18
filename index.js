7f5a60889308691c8b9a20c2149d803f87167cb9
M V <vahyno+learn@gmail.com>
2017-09-18 05:07:13 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -7,5 +7,5 @@ function nestedTarget() {
 }
 
 function deepestChild(){
-  return document.querySelector('div.grand-node div div div div')
+  return document.querySelector('#grand-node div div div div')
 }
