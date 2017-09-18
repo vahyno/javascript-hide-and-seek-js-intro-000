4c4ebcaeab72cb1d0c351368688c4dcb0aab6830
M V <vahyno+learn@gmail.com>
2017-09-18 05:15:49 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -9,3 +9,13 @@ function nestedTarget() {
 function deepestChild(){
   return document.querySelector('#grand-node div div div div')
 }
+
+function increaseRankBy(n){
+  const rankedLists= document.querySelectorAll('.ranked-list')
+  for (let i = 0; i < rankedLists.length; i++){
+    let children = rankedLists[i].children
+    for (let j =0; j<children.length; j++){
+      children[j].innerHTML = parseInt(children[j].innerHTML)+n;
+    }
+  }
+}
